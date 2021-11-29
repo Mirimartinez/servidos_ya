bcf09fca39c44b3dbf2acb260ba3884b46ef0e59
VRralf <vrralf@gmail.com>
2021-11-29 14:43:25 -0300

    Front actualizado

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SisVen</title>
+
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<!-- NAVBAR -->
+
+<header>
+    <div class="row">
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand align-items-center" href="#">
+                <img class="ms-3 me-2" src="https://img2.freepng.es/20180424/how/kisspng-computer-icons-point-of-sale-business-caddie-5adee3836533c7.3439769015245566754145.jpg" height="30" class="d-inline-block align-top" alt="">
+                Servidos YA!
+            </a>
+        </nav>
+    </div>
+</header>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,7 @@
+@include('includes.header')
+
+
+<h1>Hola Chicos!</h1>
+
+
+@include('includes.footer')
