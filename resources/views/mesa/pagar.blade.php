5662eda5436aad2c6f7a6a3ab54f2ba6b3a61818
Mirimartinez <mirianamartinezok@gmail.com>
2021-12-28 00:23:44 -0300

    boton volver en todas las vistas

--- resources/views/mesa/pagar.blade.php
+++ resources/views/mesa/pagar.blade.php
@@ -1,3 +1,12 @@
-@include('includes.header')
+@include('layouts.app')
+<div class="d-inline-flex ms-4">
+    <a href="http://localhost/proyecto_curso_utn/servidos_ya/public/mesa"><i class="fas fa-arrow-left h2"></i></a>
+</div>
+<div class="container-fluid text-center">
+    <div class="row">
+        <div class="col">
 <h1>PAGAR MESA N° {{$mesa->id}}</h1>
+        </div>
+    </div>
+</div>
 @include('includes.footer')
